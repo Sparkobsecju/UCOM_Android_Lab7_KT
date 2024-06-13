774d094e5f49697668386aeb794c836bb9ac0636
Sivan Chen <ryan1825114@gmail.com>
2024-06-13 14:01:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lab7_kt/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.lab7_kt
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu1, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.item1 -> Toast.makeText(
+                this,
+                "menu1 clicked", Toast.LENGTH_SHORT
+            ).show()
+
+            R.id.item3 -> Toast.makeText(this, "menu3 clicked", Toast.LENGTH_SHORT).show()
+            R.id.item4 -> Toast.makeText(this, "menu4 clicked", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
