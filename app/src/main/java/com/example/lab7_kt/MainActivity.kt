908d327bf22b080860f3833c1b04c50601c5526b
Sivan Chen <ryan1825114@gmail.com>
2024-06-13 14:23:18 +0800

    add menu for each View (ImageView and TextView)

--- app/src/main/java/com/example/lab7_kt/MainActivity.kt
+++ app/src/main/java/com/example/lab7_kt/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.lab7_kt
 
 import android.os.Bundle
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -19,6 +23,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val tv1: TextView = findViewById(R.id.textView)
+        val iv1: ImageView = findViewById(R.id.imageView)
+        registerForContextMenu(tv1)
+        registerForContextMenu(iv1)
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        var title=""
+        super.onCreateContextMenu(menu, v, menuInfo)
+        when(v!!.id) {
+            R.id.textView -> title = "文字元件的選單"
+            R.id.imageView -> title = "影像元件的選單"
+        }
+        menu!!.add("本文選單1")
+        menu.add("本文選單2")
+        menu.setHeaderTitle("是${title}")
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
